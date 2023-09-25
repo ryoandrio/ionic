bf3b88c1b7783d644f0ffedbd9de936b3940f604
ryoandrio <112672727+ryoandrio@users.noreply.github.com>
2023-09-25 03:30:45 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  map!: L.Map;
+
+  constructor() {}
+  // ngOnInit() {
+  // }
+
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([35.76943, -5.80081], 10)
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
+}
